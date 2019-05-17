b36d31b8015382c840d309872557065cb2187a56
Sam Lanning <sam@samlanning.com>
2019-05-16 22:15:12 -0700

    Update get-package-info.ts

--- src/backend/get-package-info.ts
+++ src/backend/get-package-info.ts
@@ -5,5 +5,5 @@ import {getInfo} from './';
 export default async (req: Request, res: Response) => {
   console.log(req.url);
   const info = await getInfo('express');
-  res.end('result: ' + JSON.stringify(info));
+  res.end('result 2: ' + JSON.stringify(info));
 };
