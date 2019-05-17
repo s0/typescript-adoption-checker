34866c2ac2186e48a0943617389f130a82f67d37
Sam Lanning <sam@samlanning.com>
2019-05-16 19:18:35 -0700

    Initial implementation getting data

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,72 @@
 import * as npm from '@samlanning/npm-registry-client';
 
-const PACKAGES = ['npm-registry-client', '@synesthesia-project/core', 'tree-sitter', 'express'];
+const PACKAGES = ['npm-registry-client', '@synesthesia-project/light-desk', 'tree-sitter', 'express', 'something-non-existent'];
 
 console.log ('hello world');
 
-async function getInfo(pkg: string) {
-  console.log('loading package info: ', pkg);
+type PackageInfo =
+{
+  result: 'error';
+  message: string;
+} | {
+  result: 'success';
+  types: 'included' | 'definitely-typed' | 'none';
+};
 
-  const data = await npm.getPackageMeta(pkg);
-  console.log(data);
+/**
+ * Fetch the latest typescript information about a package
+ *
+ * @param pkg the name of the npm package
+ */
+async function getInfo(pkg: string): Promise<PackageInfo> {
+
+  const data = await npm.getPackageMeta(pkg).catch(err => {
+    if (err.statusCode !== 404) console.error(err);
+    return null;
+  });
+
+  if (!data) return {
+    result: 'error',
+    message: 'Error getting package info'
+  };
+
+  const latest = data['dist-tags'].latest;
+  const version = data.versions[latest];
+  if (!version) return {
+    result: 'error',
+    message: 'Missing version'
+  };
+  if (!!version.types) {
+    return {
+      result: 'success',
+      types: 'included'
+    };
+  } else {
+    // Check definitely typed
+    const dtPackage = await npm.getPackageMeta('@types/' + pkg).catch(err => {
+      if (err.statusCode !== 404) console.error(err);
+      return null;
+    });
+    if (dtPackage) {
+      return {
+        result: 'success',
+        types: 'definitely-typed'
+      };
+    } else {
+      return {
+        result: 'success',
+        types: 'none'
+      };
+    }
+  }
 
 
 }
 
+async function printInfo(pkg: string) {
+  const info = await getInfo(pkg);
+  console.log(pkg, info);
+}
 
-PACKAGES.map(getInfo);
+
+PACKAGES.map(printInfo);
