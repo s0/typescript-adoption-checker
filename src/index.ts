dbfc22fd489d655a34cff8b2b9689784ddf849bc
Sam Lanning <sam@samlanning.com>
2019-05-16 18:59:05 -0700

    Start using external npm client library

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import * as npm from '@samlanning/npm-registry-client';
+
+const PACKAGES = ['npm-registry-client', '@synesthesia-project/core', 'tree-sitter', 'express'];
+
 console.log ('hello world');
+
+async function getInfo(pkg: string) {
+  console.log('loading package info: ', pkg);
+
+  const data = await npm.getPackageMeta(pkg);
+  console.log(data);
+
+
+}
+
+
+PACKAGES.map(getInfo);
